69b5c7abb5a3a25afff8cf4a90449e4541188eec
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-02-08 17:52:35 +0200

    Posting a Request to Server

--- Public/scoring-script.js
+++ Public/scoring-script.js
@@ -1,5 +1,6 @@
 const updateButton = document.getElementById("UpdateButton");
 
+// Check validity of password
 function checkPassword() {
     if (document.getElementById("Password").value === "4127") {
         return true;
@@ -9,9 +10,36 @@ function checkPassword() {
     }
 }
 
+// Update scores
 function update() {
     if (checkPassword() === true) {
         console.log("correct password");
+
+        // Retrieve all information
+        const field = document.getElementById("Field").value;
+        const bowlingTeam = document.getElementById("BowlingTeam").value;
+        const battingTeam = document.getElementById("BattingTeam").value;
+        const target = document.getElementById("Target").value;
+        const total = document.getElementById("Total").value;
+        const wickets = document.getElementById("Wickets").value;
+        const ballsRemaining = document.getElementById("BallsRemaining").value;
+        const batsman1 = document.getElementById("Batsman1").value;
+        const runs1 = document.getElementById("Runs1").value;
+        const batsman2 = document.getElementById("Batsman2").value;
+        const runs2 = document.getElementById("Runs2").value;
+
+        const scores = {total, wickets};
+        const data = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(total, wickets)
+        };
+
+        console.log('loaded');
+
+        fetch('/api', data);
+
+        console.log('fetched');
     }
     else {
         console.log("incorrect password");
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+//const path = require('path');
 const app = express();
 
 // Host the data on a web server
 app.listen(3000, () => console.log('listening at port 3000'));
+//app.use(express.static(path.join(__dirname, '/Public')))
 app.use(express.static('Public'));
+
+// Information to be saved to the database
+app.post('/api', (request, response) => {
+    console.log(request);
+});
