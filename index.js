a819aec2d52592301726c36e92c02b27d349b064
taylarogers <110628845+taylarogers@users.noreply.github.com>
2023-02-08 08:37:38 +0200

    Spans with Unique IDs Inserted for Easy Editing of Data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const app = express();
+
+// Host the data on a web server
 app.listen(3000, () => console.log('listening at port 3000'));
 app.use(express.static('Public'));
